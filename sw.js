0eb79396ed29037df2656ec9ed7cfbfaf0756e9c
Aboss3b13 <A.raza.inf23@stud.bbbaden.ch>
2026-07-19 11:28:40 +0200

    Polish tablet and desktop workspace

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "abrahamic-books-v49";
+const CACHE_VERSION = "abrahamic-books-v50";
 const APP_CACHE = `${CACHE_VERSION}-app`;
 const DATA_CACHE = `${CACHE_VERSION}-data`;
 const APP_SHELL = [
