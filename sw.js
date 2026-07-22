a910062c48579f61874facf35626e4177f0699b7
Aboss3b13 <A.raza.inf23@stud.bbbaden.ch>
2026-07-23 01:02:01 +0200

    Add editable note tags and Windows-style folders

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "abrahamic-books-v54";
+const CACHE_VERSION = "abrahamic-books-v55";
 const APP_CACHE = `${CACHE_VERSION}-app`;
 const DATA_CACHE = `${CACHE_VERSION}-data`;
 const APP_SHELL = [
